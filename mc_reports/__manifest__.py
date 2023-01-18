8551f4fbe60f9081481795c47b83a1c68b900842
aqibsheikh <sheikhaqib891@gmail.com>
2023-01-18 18:22:30 +0500

    reports

---
+++ mc_reports/__manifest__.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+{
+    "name": "mc_reports",
+    "version": "15.0.0.0",
+    "category": "Projects",
+    "author": "Mediod Consulting",
+    "price": 50.00,
+    'currency': "USD",
+    'summary': '',
+    "website": "https://mediodconsulting.com/",
+    "data": [
+        'reports/custom_sale_report.xml',
+        'reports/custom_purchase_order.xml',
+        'reports/report.xml',
+        'views/invoice_inherit.xml',
+        'views/custom_internal_layout.xml',
+        'views/sale_report.xml',
+        'views/purchase_order.xml',
+        'views/custom_purchase_footer.xml',
+    ],
+    "auto_install": False,
+    "installable": True,
+    'license': 'OPL-1',
+}
